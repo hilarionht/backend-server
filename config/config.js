2047f112aed5259693c0b4a6feb8cb635fbd78b9
hilarionht <trejo.hilarion@gmail.com>
2018-01-28 19:13:27 -0300

    fin del backen

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'thiago_213900%_mivida';
+
+module.exports.GOOGLE_CLIENT_ID = '377013586154-njrbcn0itlm441q75ijmd6sgfvqnenvi.apps.googleusercontent.com';
+
+module.exports.GOOGLE_SECRET = 'D5LCjCMowxO7oG1Ur12KtVA-';
--- routes/login.js
+++ routes/login.js
@@ -6,9 +6,104 @@ var bcrypt = require('bcryptjs');
 
 var jwt = require('jsonwebtoken');
 
+//var GoogleAuth = require('google-auth-library');
+
 var SEED = require('../config/config').SEED;
 
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
 var Usuario = require('../models/usuario');
+
+//============AUTENTICACION GOOGLE =================
+var GoogleAuth = require('google-auth-library');
+var auth = new GoogleAuth;
+var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+app.post('/google', (req, res, next) => {
+
+    var token = req.body.token || 'fff';
+    // var client = new OAuth2(
+    //     GOOGLE_CLIENT_ID,
+    //     GOOGLE_SECRET,
+    //     ''
+    // );
+    var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+    client.verifyIdToken(
+        token,
+        GOOGLE_CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
+        function(e, login) {
+            if (e) {
+                return res.status(400).json({ mensaje: 'token no valido', ok: false, cliente: client });
+            }
+            var payload = login.getPayload();
+            var userid = payload['sub'];
+
+            Usuario.findOne({ email: payload.email }, (err, usuario) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'error al buscar usuario - login',
+                        errors: err
+                    });
+                }
+                if (usuario) {
+                    if (usuario.google === false) {
+                        return res.status(400).json({
+                            ok: false,
+                            mensaje: 'Debe usar su autenticacion normal.'
+                        });
+                    } else {
+                        usuario.password = ':)';
+                        var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }) //4 horas
+                        res.status(200).json({
+                            ok: true,
+                            usuario: usuario,
+                            token: token,
+                            id: usuario._id
+                        });
+                    }
+                    //si usuario no existe por correo
+                } else {
+                    var usuario = new Usuario();
+                    usuario.nombre = payload.name;
+                    usuario.email = payload.email;
+                    usuario.password = '****';
+                    usuario.img = payload.picture;
+                    usuario.google = true;
+                    usuario.save((err, usuarioSave) => {
+                        if (err) {
+                            return res.status(500).json({
+                                ok: false,
+                                mensaje: 'error al crear usuario - login',
+                                errors: err
+                            });
+                        }
+                        var token = jwt.sign({ usuario: usuarioSave }, SEED, { expiresIn: 14400 }) //4 horas
+                        res.status(200).json({
+                            ok: true,
+                            usuario: usuarioSave,
+                            token: token,
+                            id: usuarioSave._id
+                        });
+                    });
+                }
+            });
+            // If request specified a G Suite domain:
+            //var domain = payload['hd'];
+            //res.status(200).json({ mensaje: 'OK', ok: true, payload: payload });
+        }
+    );
+
+
+
+});
+//============AUTENTICACION GOOGLE =================
+
+
 
 app.post('/', (req, res) => {
 
